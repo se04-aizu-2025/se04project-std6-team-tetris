bcfdba574adc2ed3a3832b92cf2992bfeacae765
AnriSakamaki <anri1nohime@gmail.com>
2025-12-18 17:16:47 +0900

    Add 3 sorting algorithms

---
+++ Backend/sort_file/selection.c
@@ -0,0 +1,23 @@
+void selection(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+
+        for (int j = i + 1; j < n; j++) {
+            log_compare(j, min_index); 
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }else {
+                log_noswap(j, min_index);       // 交換なしログ
+            }
+        }
+
+        // 最小値を先頭と交換
+        if (min_index != i) {
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+
+            log_swap(i, min_index, arr);
+        }
+    }
+}

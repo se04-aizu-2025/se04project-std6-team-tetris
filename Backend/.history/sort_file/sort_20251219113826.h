76e9be9e95c0a9b6cea0ad0587a9b650f316f120
Anri <131333244+AnriSakamaki@users.noreply.github.com>
2026-02-03 12:54:04 +0900

    Delete Backend/.history directory

--- Backend/.history/sort_file/sort_20251219113826.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef SORT_H
-#define SORT_H
-
-void selection(int arr[], int n);
-void marge(int arr[], int left, int mid, int right);
-void gnome(int arr[], int n);
-
-#endif // MY_FUNCTIONS_H
